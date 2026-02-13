3b04450d5ffb3eb71823092872c92224c8493730
stutie <stutieagrawaal@gmail.com>
2026-02-13 22:43:44 +0530

    hello

---
+++ backend/vercel (1).js
@@ -0,0 +1,8 @@
+{
+  "version": 2,
+  "rewrites": [
+    { "source": "/api/(.*)", "destination": "/server.js" },
+    { "source": "/signup", "destination": "/public/signup.html" },
+    { "source": "/signin", "destination": "/public/signin.html" }
+  ]
+}
